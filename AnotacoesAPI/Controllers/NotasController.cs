47543ad3a2d276ebbf59e748bf65685b7d0fdd85
Gustavo Pereira Pascutti <gustavo.pascutti@ibopedtm.com>
2019-10-10 15:14:56 -0300

    Add project files.

---
+++ AnotacoesAPI/Controllers/NotasController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnotacoesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace AnotacoesAPI.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            MongoDbContext dbContext = new MongoDbContext();
+            List<Nota> listaNotas = dbContext.Notas.Find(m => true).ToList();
+            return View(listaNotas);
+        }
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            MongoDbContext dbContext = new MongoDbContext();
+            var entity = dbContext.Notas.Find(m => m.Id == id).FirstOrDefault();
+            return View(entity);
+        }
+        [HttpPost]
+        public IActionResult Edit(Nota entity)
+        {
+            MongoDbContext dbContext = new MongoDbContext();
+            dbContext.Notas.ReplaceOne(m => m.Id == entity.Id, entity);
+            return View(entity);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Nota entity)
+        {
+            MongoDbContext dbContext = new MongoDbContext();
+            entity.Id = Guid.NewGuid();
+            dbContext.Notas.InsertOne(entity);
+            return RedirectToAction("Index", "Notas");
+        }
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            MongoDbContext dbContext = new MongoDbContext();
+            dbContext.Notas.DeleteOne(m => m.Id == id);
+            return RedirectToAction("Index", "Notas");
+        }
+    }
+}
---
+++ AnotacoesAPI/Models/Nota.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnotacoesAPI.Models
+{
+    public class Nota
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Conteudo { get; set; }
+        [Required]
+        public int Acessos { get; set; }
+    }
+}
